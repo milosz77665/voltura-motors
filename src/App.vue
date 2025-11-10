336d692ed52b88c3c68de21a809b7247e2bf94e6
Miłosz Marchewka <milosz77665@gmail.com>
2025-11-10 09:16:19 +0100

    add Hero component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import Header from "./components/Header.vue";
+import Hero from "./components/Hero.vue";
 </script>
 
 <template>
   <Header />
+  <main class="container mt-4 d-flex flex-column gap-5">
+    <Hero />
+  </main>
 </template>
---
+++ src/components/Hero.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="row p-4 bg-white rounded-4 border mx-0">
+    <div class="col-12 d-xl-none">
+      <h1 class="fw-bold">Moc spotyka efektywność</h1>
+    </div>
+    <div class="col-12 d-flex flex-column justify-content-between col-lg-6">
+      <div>
+        <h1 class="fw-bold d-none d-xl-block">Moc spotyka efektywność</h1>
+        <div class="d-flex justify-content-center align-items-center mb-3 d-lg-none">
+          <img src="../assets/images/car_hero.webp" class="img-fluid" alt="Samochód Voltura" />
+        </div>
+        <p class="text-body-secondary">
+          Odkryj nową linię Voltura e-Line – zaprojektowaną z myślą o mieście i długich trasach
+        </p>
+        <p class="text-body-tertiary">
+          Voltura e-Line to zupełnie nowa generacja samochodów elektrycznych, łączących dynamiczne osiągi z wyjątkową
+          energooszczędnością.
+          <br />
+          Zaprojektowana w oparciu o najnowsze technologie, oferuje nie tylko imponujący zasięg, ale też komfort jazdy
+          na najwyższym poziomie.
+          <br />
+          Nowoczesne systemy wspomagania kierowcy sprawiają, że każda podróż staje się bezpieczniejsza i bardziej
+          intuicyjna.
+          <br />
+          Elegancka sylwetka i dopracowane detale nadwozia podkreślają charakter premium marki Voltura.
+        </p>
+      </div>
+      <div>
+        <button class="btn btn-primary px-4">Sprawdź szczegóły</button>
+      </div>
+    </div>
+    <div class="d-none d-lg-flex col-lg-6 justify-content-lg-center align-self-lg-start">
+      <img src="../assets/images/car_hero.webp" class="img-fluid" alt="Samochód Voltura" />
+    </div>
+  </section>
+</template>
