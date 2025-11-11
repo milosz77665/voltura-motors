5886d551c467a5d05d98efadc12713e74301fb02
Miłosz Marchewka <milosz77665@gmail.com>
2025-11-11 16:26:15 +0100

    import bootstrap js

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import "./scss/custom.scss";
 import App from "./App.vue";
 import fixOrphans from "./directives/fixOrphans";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const app = createApp(App);
 app.directive("fix-orphans", fixOrphans);
