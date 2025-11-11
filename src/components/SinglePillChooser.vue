a8ca10548b1dcf873d3d824dc06735d4bc3840a1
Miłosz Marchewka <milosz77665@gmail.com>
2025-11-11 16:19:49 +0100

    add SinglePillChooser

---
+++ src/components/SinglePillChooser.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const chosenOption = defineModel<string>();
+defineProps<{ availableOptions: string[] }>();
+
+function onClick(option: string) {
+  chosenOption.value = option;
+}
+</script>
+
+<template>
+  <div class="nav nav-pills row gy-2" role="radiogroup">
+    <div v-for="option of availableOptions" class="nav-item col">
+      <button
+        role="radio"
+        class="nav-link small text-center border w-100"
+        :class="{ active: option === chosenOption, 'text-black': option !== chosenOption }"
+        @click="onClick(option)"
+        :aria-checked="option === chosenOption"
+      >
+        {{ option }}
+      </button>
+    </div>
+  </div>
+</template>
